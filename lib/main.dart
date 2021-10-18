2c1f956f1effa306c93f215ed9e10d27dd91d889
javierrod9191 <javierrod9191@gmail.com>
2021-10-18 18:19:15 -0500

    chore: add MyApp class

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
-
-
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => const MaterialApp(
+        home: Center(
+      child: Text('javier'),
+    ));
+}
