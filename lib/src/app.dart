61ca1344872d49ea2a1a9c14616dc1c4c264766e
javierrod9191 <javierrod9191@gmail.com>
2021-10-18 20:35:19 -0500

    chore: add debugShowCheckedModeBanner to false

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -5,6 +5,7 @@ import 'package:test1/src/pages/counter_page.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Center(
           child: CounterPage(),
           // child: HomePage(),
