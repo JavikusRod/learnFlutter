619084aa1bf2c2f0287ce269bb6159cf4e540b1e
javierrod9191 <javierrod9191@gmail.com>
2021-10-19 18:57:39 -0500

    chore: add MaterialApp+ HomePageTemp on body

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Components',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text('Components'),
+        ), body: HomePageTemp(),
+      ),
+    );
+  }
+
+
