3b9341d528b42441e360ae7e4caab8b486f5e750
javierrod9191 <javierrod9191@gmail.com>
2021-10-18 19:12:19 -0500

    chore: add floating button

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -4,25 +4,27 @@ class HomePage extends StatelessWidget {
   TextStyle textStyle = TextStyle(fontSize: 22.0);
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Javier testing flutter'),
-          centerTitle: true,
-          elevation: 4,
+      appBar: AppBar(
+        title: const Text('Javier testing flutter'),
+        centerTitle: true,
+        elevation: 4,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Numero de clics',
+              style: textStyle,
+            ),
+            Text(
+              '0',
+              style: textStyle,
+            ),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Numero de clics',
-                style: textStyle,
-              ),
-              Text(
-                '0',
-                style: textStyle,
-              ),
-            ],
-          ),
-        ),
-      );
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+      ));
 }
