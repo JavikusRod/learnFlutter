ac070bcfba2939c058156fd2bcd164896a2d198f
javierrod9191 <javierrod9191@gmail.com>
2021-10-20 11:54:00 -0500

    chore: add card route

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:test1/src/pages/alert_page.dart';
 import 'package:test1/src/pages/avatar_page.dart';
+import 'package:test1/src/pages/card_page.dart';
 import 'package:test1/src/pages/home_page.dart';
 
 Map<String, WidgetBuilder> getAppRoutes() => <String, WidgetBuilder>{
       '/': (BuildContext context) => HomePage(),
       'alert': (BuildContext context) => AlertPage(),
       'avatar': (BuildContext context) => AvatarPage(),
+      'card': (BuildContext context) => CardPage(),
     };
 
 MaterialPageRoute notFoundRoute(settings) => MaterialPageRoute(
@@ -17,9 +19,7 @@ MaterialPageRoute notFoundRoute(settings) => MaterialPageRoute(
           title: Text('Not Found Page'),
         ),
         body: Center(
-          child: Container(
-            child: Text('Not Found Page'),
-          ),
+          child: Text('Not Found Page'),
         ),
       ),
     );
