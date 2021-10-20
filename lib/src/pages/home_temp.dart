2a8db8c839e3b0f19146837461b15c00b78c8669
javierrod9191 <javierrod9191@gmail.com>
2021-10-19 21:17:50 -0500

    chore: add list from a List

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,36 +1,35 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final options = [
+    'Testing list',
+    'Good Observations',
+    'Nice Saves',
+    'Collapse God'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Components Temp'),
       ),
-      body: ListView(
-        children: const [
-          ListTile(
-            title: Text('Testing list Tile'),
-            subtitle: Text('Cool Subtilte'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('Testing list Tile 1 '),
-            dense: true,
-          ),
-          Divider(),
-          ListTile(
-            title: Text('Testing list Tile 2'),
-            enabled: false,
-          ),
-          Divider(),
-          ListTile(
-            title: Text('Testing list Tile 3'),
-            selected: true,
-            selectedTileColor: Colors.deepPurpleAccent,
-          )
-        ],
-      ),
+      body: ListView(children: _createItems()),
     );
   }
+
+  List<Widget> _createItems() => options
+      .map((element) => Column(
+            children: [
+              ListTile(
+                title: Text(element + '!'),
+                subtitle: Text('Something to say'),
+                leading: Icon(Icons.picture_in_picture),
+                trailing: Icon(Icons.add_box_outlined),
+                onTap: () {},
+              ),
+              const Divider(),
+            ],
+          ))
+      .toList();
 }
