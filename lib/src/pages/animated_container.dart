6889cba132c16406c3b0dd9858c34c4ea9495d8a
javierrod9191 <javierrod9191@gmail.com>
2021-10-21 13:35:53 -0500

    chore: add container initial values

--- lib/src/pages/animated_container.dart
+++ lib/src/pages/animated_container.dart
@@ -6,12 +6,27 @@ class AnimatedContainerPage extends StatefulWidget {
 }
 
 class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
+  double _width = 50.0;
+  double _height = 50.0;
+  Color _color = Colors.pink;
+
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8.0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(' Animated Container'),
         ),
-        body: Text('TESTED'));
+        body: Center(
+          child: Container(
+            width: _width,
+            height: _height,
+            decoration: BoxDecoration(
+              borderRadius: _borderRadius,
+              color: _color,
+            ),
+          ),
+        ));
   }
 }
