45675b41b8444665826fe5c38cf87a30a3ed4111
javierrod9191 <javierrod9191@gmail.com>
2021-10-21 11:03:20 -0500

    chore: add Circle Avatar + styles

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,10 +6,32 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                'https://avatars.githubusercontent.com/u/16100843?v=4',
+              ),
+              radius: 20,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 15, left: 15),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
       ),
       body: Center(
-        child: Container(
-          child: Text('Avatar Page'),
+        child: FadeInImage(
+          image: NetworkImage(
+            'https://avatars.githubusercontent.com/u/16100843?v=4',
+          ),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
         ),
       ),
     );
