295c39d6def92c21fae286f4f17c93322f401f4c
javierrod9191 <javierrod9191@gmail.com>
2021-10-21 15:17:23 -0500

    chore: add styles to the input

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -8,6 +8,25 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Inputs'),
+        ),
+        body: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+          children: [_createInput()],
+        ));
+    ;
   }
 }
+
+_createInput() => TextField(
+    // autofocus: true,
+    textCapitalization: TextCapitalization.sentences,
+    decoration: InputDecoration(
+        label: Text('Stuff'),
+        hintText: 'Write stuff',
+        counter: Text('qty of letters'),
+        suffixIcon: Icon(Icons.title),
+        icon: Icon(Icons.import_contacts_sharp),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))));
