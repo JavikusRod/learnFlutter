b31a7254add75b025cdb3ce0f9caa5da22775d2a
javierrod9191 <javierrod9191@gmail.com>
2021-10-21 10:41:26 -0500

    chore: add AlertDialog

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -8,9 +8,41 @@ class AlertPage extends StatelessWidget {
         title: Text('Alert Page'),
       ),
       body: Center(
-        child: Container(
-          child: Text('Alert Page'),
+        child: ElevatedButton(
+          onPressed: () => _showAlert(context),
+          child: Text('Alert Button'),
+          style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)))),
         ),
+      ),
+    );
+  }
+
+  _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        title: Text('This is an Alert'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Content of the Dialog'),
+            FlutterLogo(
+              size: 100,
+            )
+          ],
+        ),
+        actions: [
+          ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(), child: Text('OK')),
+          ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Cancel'))
+        ],
       ),
     );
   }
