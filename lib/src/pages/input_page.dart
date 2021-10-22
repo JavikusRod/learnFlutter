8584d74cc10fd582da91ed2b7168b7a5b6be0b25
javierrod9191 <javierrod9191@gmail.com>
2021-10-21 21:20:29 -0500

    chore: add inputs for password & mail

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -7,6 +7,8 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   late String _stuff = '';
+  late String _email = '';
+  late String _password = '';
 
   @override
   Widget build(BuildContext context) {
@@ -18,26 +20,57 @@ class _InputPageState extends State<InputPage> {
           padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
           children: [
             _createInput(),
+            Divider(),
+            _createMail(),
+            Divider(),
+            _createPassword(),
+            Divider(),
             ListTile(
               title: Text('Stuff written: $_stuff'),
-            )
+              subtitle: Text('Email: $_email $_password'),
+            ),
           ],
         ));
   }
 
   _createInput() => TextField(
-      onChanged: (value) {
-        setState(() {
-          _stuff = value;
-        });
-      },
+      onChanged: (value) => setState(() {
+            _stuff = value;
+          }),
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
           label: Text('Stuff'),
           hintText: 'Write stuff',
           counter: Text('qty of letters ${_stuff.length}'),
-          suffixIcon: Icon(Icons.title),
           icon: Icon(Icons.import_contacts_sharp),
+          suffixIcon: Icon(Icons.title),
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))));
+
+  _createMail() => TextField(
+      onChanged: (value) => setState(() {
+            _email = value;
+          }),
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          label: Text('E-Mail'),
+          hintText: 'E-Mail',
+          icon: Icon(Icons.mail),
+          suffixIcon: Icon(Icons.alternate_email),
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))));
+
+  _createPassword() => TextField(
+      onChanged: (value) => setState(() {
+            _password = value;
+          }),
+      obscureText: true,
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          label: Text('Password'),
+          hintText: 'Password',
+          icon: Icon(Icons.lock),
+          suffixIcon: Icon(Icons.lock_open),
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))));
 }
