8a7b90a3583acd503ba473fb39a4e021adb04dec
javierrod9191 <javierrod9191@gmail.com>
2021-10-23 16:07:53 -0500

    refactor: remove unssed files for the new app

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,50 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:test1/src/providers/menu_provider.dart';
-import 'package:test1/src/utils/icon_string_util.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('The Home Page'),
-      ),
-      body: _list(),
+      body: SafeArea(child: Text('Home Page')),
     );
-  }
-
-  Widget _list() {
-    return FutureBuilder(
-      future: menuProvider.loadData(),
-      // initialData: [],
-      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
-        return ListView(children: _listView(snapshot.data, context));
-      },
-    );
-  }
-
-  List<Widget> _listView(List<dynamic>? data, BuildContext context) {
-    //Need o solve the null possibility
-    final List<Widget> options = [];
-    if (data != null) {
-      data.forEach((element) {
-        final widgetTemp = ListTile(
-          title: Text(element['text']),
-          onTap: () {
-            Navigator.pushNamed(context, element['route']);
-          },
-          leading: getIcon(iconName: element['icon']),
-          trailing: Icon(
-            Icons.keyboard_arrow_right,
-            color: Colors.red,
-          ),
-        );
-        options
-          ..add(widgetTemp)
-          ..add(Divider());
-      });
-      return options;
-    }
-    return options;
   }
 }
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,23 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:test1/src/pages/alert_page.dart';
-import 'package:test1/src/pages/animated_container.dart';
-import 'package:test1/src/pages/avatar_page.dart';
-import 'package:test1/src/pages/card_page.dart';
 import 'package:test1/src/pages/home_page.dart';
-import 'package:test1/src/pages/input_page.dart';
-import 'package:test1/src/pages/list_page.dart';
-import 'package:test1/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getAppRoutes() => <String, WidgetBuilder>{
       '/': (BuildContext context) => HomePage(),
-      'alert': (BuildContext context) => AlertPage(),
-      'avatar': (BuildContext context) => AvatarPage(),
-      'card': (BuildContext context) => CardPage(),
-      'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
-      'inputs': (BuildContext context) => InputPage(),
-      'slider': (BuildContext context) => SliderPage(),
-      'list': (BuildContext context) => ListPage(),
     };
 
 MaterialPageRoute notFoundRoute(settings) => MaterialPageRoute(
