5550bb08646a461cf7e06a4ea8727dd7626dc39d
javierrod9191 <javierrod9191@gmail.com>
2021-10-23 17:30:39 -0500

    chore: add CardSwiper + required property list

---
+++ lib/src/widgets/card_swiper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CardSwiper extends StatelessWidget {
+  late final List<dynamic> list;
+
+  CardSwiper({required this.list});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
