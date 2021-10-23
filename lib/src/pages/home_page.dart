e61d2f81c43a44dba5792c4ccf84f85cb3cdc8d9
javierrod9191 <javierrod9191@gmail.com>
2021-10-23 16:38:27 -0500

    chore: add app bar+ search icon+ cardSwiper method

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -4,7 +4,19 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Home Page')),
+      appBar: AppBar(
+        title: Text(' Movie Search'),
+        centerTitle: false,
+        backgroundColor: Colors.indigoAccent,
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
+      ),
+      body: Container(
+        child: Column(
+          children: [_cardSwiper()],
+        ),
+      ),
     );
   }
+
+  Container _cardSwiper() => Container();
 }
