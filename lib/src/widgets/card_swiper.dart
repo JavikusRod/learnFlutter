3feb506f584d6ce9a9a3481497c46179dc51e3c8
javierrod9191 <javierrod9191@gmail.com>
2021-10-23 17:35:23 -0500

    chore: add swiper to the widget

--- lib/src/widgets/card_swiper.dart
+++ lib/src/widgets/card_swiper.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class CardSwiper extends StatelessWidget {
   late final List<dynamic> list;
@@ -6,7 +7,18 @@ class CardSwiper extends StatelessWidget {
   CardSwiper({required this.list});
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  Widget build(BuildContext context) => Container(
+        padding: EdgeInsets.only(top: 10),
+        width: double.infinity,
+        height: 400,
+        child: Swiper(
+          layout: SwiperLayout.STACK,
+          itemWidth: 300,
+          itemCount: 4,
+          itemBuilder: (BuildContext context, int index) => Image.asset(
+            'assets/no-image.jpg',
+            fit: BoxFit.fill,
+          ),
+        ),
+      );
 }
